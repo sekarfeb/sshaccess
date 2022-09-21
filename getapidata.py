4f3de3e59ff55ba59469de09765df2671cc96691
sekarfeb <sekarfeb@gmail.com>
2022-09-21 16:10:20 +0530

    Update getapidata.py

--- getapidata.py
+++ getapidata.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
 import requests
-api_key = os.environ['API_KEY']response = requests.get("https://thedogapi.com/v1/breeds?api_key={}".format(api_key))
+api_key = os.environ['API_KEY']
+response = requests.get("https://thedogapi.com/v1/breeds?api_key={}".format(api_key))
 print(response)
